fcaba6f4e121acff91eeb8387381c7874e2aa3a5
Teodora Baluta <teobaluta@gmail.com>
2021-01-23 18:47:39 +0800

    Add installation instructions for dep pixeldp

--- install.sh
+++ install.sh
@@ -12,3 +12,15 @@ export PROVERO_MODELS_PATH=/tmp/
 
 # Datasets (MNIST, CIFAR10, ImageNet) directory
 export DATA_DIR=./DATASET_DIR
+
+DIR=$(pwd)
+cd $HOME
+git clone https://github.com/columbia/pixeldp
+cd pixeldp
+wget  http://www.cs.columbia.edu/~mathias/pixeldp/cifar10.zip
+unzip cifar10.zip
+mkdir -p trained_models/pixeldp_resnet_attack_norm_l2_size_0.1_1_prenoise_layers_sensitivity_l2_scheme_bound_activation_postnoise
+cp cifar10/* trained_models/pixeldp_resnet_attack_norm_l2_size_0.1_1_prenoise_layers_sensitivity_l2_scheme_bound_activation_postnoise
+cd $DIR
+
+export PIXELDP_HOME=$HOME/pixeldp
